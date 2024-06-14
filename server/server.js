32b231b0d135d2bbeb11f5ebf2d7d8744afa94f0
Jessy Castaneda <jessyc.12@icloud.com>
2024-06-13 18:13:55 -0700

    push code

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const { authMiddleware } = require('./utils/auth');
-const path = require('path');
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { authMiddleware } = require("./utils/auth");
+const path = require("path");
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -20,25 +20,27 @@ const startApolloServer = async () => {
 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 
-  app.use('/graphql', expressMiddleware(server, { context: authMiddleware }));
-  
+  app.use("/graphql", expressMiddleware(server, { context: authMiddleware }));
+
   // if we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
   }
 
-  db.once('open', () => {
+  db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`🌍 Now listening on localhost:${PORT}`);
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
